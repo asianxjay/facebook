ff7f20e93d119a7197d53c1eb4552e3aaf0ce327
asianxjay <jtmd@writtenfell-lm.corp.yahoo.com>
2014-06-09 10:55:58 -0700

    Homework Week 1
    Facebook Post

---
+++ facebook/mainViewController.m
@@ -0,0 +1,97 @@
+//
+//  mainViewController.m
+//  facebook
+//
+//  Created by Jason Demetillo on 6/9/14.
+//  Copyright (c) 2014 codepath. All rights reserved.
+//
+
+#import "mainViewController.h"
+
+@interface mainViewController ()
+@property (strong, nonatomic) IBOutlet UIView *textField;
+
+- (IBAction)onTap:(id)sender;
+
+- (void)willShowKeyboard:(NSNotification *)notification;
+- (void)willHideKeyboard:(NSNotification *)notification;
+
+@end
+
+@implementation mainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Register the methods for the keyboard hide/show events
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(willShowKeyboard:) name:UIKeyboardWillShowNotification object:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(willHideKeyboard:) name:UIKeyboardWillHideNotification object:nil];
+    }
+    return self;
+}
+
+- (void)willShowKeyboard:(NSNotification *)notification {
+    NSDictionary *userInfo = [notification userInfo];
+    
+    // Get the keyboard height and width from the notification
+    // Size varies depending on OS, language, orientation
+    CGSize kbSize = [[userInfo objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+    NSLog(@"Height: %f Width: %f", kbSize.height, kbSize.width);
+    
+    // Get the animation duration and curve from the notification
+    NSNumber *durationValue = userInfo[UIKeyboardAnimationDurationUserInfoKey];
+    NSTimeInterval animationDuration = durationValue.doubleValue;
+    NSNumber *curveValue = userInfo[UIKeyboardAnimationCurveUserInfoKey];
+    UIViewAnimationCurve animationCurve = curveValue.intValue;
+    
+    // Move the view with the same duration and animation curve so that it will match with the keyboard animation
+    [UIView animateWithDuration:animationDuration
+                          delay:0.0
+                        options:(animationCurve << 16)
+                     animations:^{
+                         self.textField.frame = CGRectMake(0, self.view.frame.size.height - kbSize.height - self.textField.frame.size.height - 69, self.textField.frame.size.width, self.textField.frame.size.height);
+                     }
+                     completion:nil];
+}
+
+- (void)willHideKeyboard:(NSNotification *)notification {
+    NSDictionary *userInfo = [notification userInfo];
+    
+    // Get the animation duration and curve from the notification
+    NSNumber *durationValue = userInfo[UIKeyboardAnimationDurationUserInfoKey];
+    NSTimeInterval animationDuration = durationValue.doubleValue;
+    NSNumber *curveValue = userInfo[UIKeyboardAnimationCurveUserInfoKey];
+    UIViewAnimationCurve animationCurve = curveValue.intValue;
+    
+    // Move the view with the same duration and animation curve so that it will match with the keyboard animation
+    [UIView animateWithDuration:animationDuration
+                          delay:0.0
+                        options:(animationCurve << 16)
+                     animations:^{
+                         self.textField.frame = CGRectMake(0, self.view.frame.size.height - self.textField.frame.size.height - 110, self.textField.frame.size.width, self.textField.frame.size.height);
+                     }
+                     completion:nil];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)onTap:(id)sender {
+    [self.view endEditing: YES];
+}
+
+
+@end
+
+
+
